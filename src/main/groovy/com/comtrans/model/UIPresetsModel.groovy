750b71da0a6a904ac758aee21b0c4e585b83cd70
IGOR <Gariklihodey@gmail.com>
2017-01-29 21:17:51 +0200

    add ui preset model

---
+++ src/main/groovy/com/comtrans/model/UIPresetsModel.groovy
@@ -0,0 +1,33 @@
+package com.comtrans.model
+
+class UIPresetsModel {
+
+    //sim 0 settings/////////
+    String sim0Apn
+    String sim0ApnUser
+    String sim0ApnPassword
+
+    //sim 1 settings/////////
+    String sim1Apn
+    String sim1ApnUser
+    String sim1ApnPassword
+
+    //server settings/////////
+    String serverURL
+    String serverPort
+
+    //uzor settings/////////
+    String sensorEnable0
+    String sensorEnable1
+    String levelMax0
+    String levelMax1
+    String dLevelMaxPrecent0
+    String dLevelMaxPrecent1
+
+    //device settings
+    String deviceID
+    String sim0MCCMNC
+    List<String> sim1MCCMNC = []
+
+    Short crc16
+}
--- src/main/groovy/com/comtrans/service/PresetParserService.groovy
+++ src/main/groovy/com/comtrans/service/PresetParserService.groovy
@@ -1,5 +1,6 @@
 package com.comtrans.service
 
+import com.comtrans.model.UIPresetsModel
 import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Service
 
@@ -24,7 +25,6 @@ class PresetParserService {
     Byte[] sim0ApnUser = new Byte[11]
     Byte[] sim0ApnPassword = new Byte[11]
 
-
     //sim 1 settings/////////
     Byte[] sim1Apn = new Byte[31]
     Byte[] sim1ApnUser = new Byte[11]
@@ -35,8 +35,8 @@ class PresetParserService {
     Byte[] serverPort = new Byte[6]
 
     //uzor settings/////////
-    Byte sebsorEnable0
-    Byte sebsorEnable1
+    Byte sensorEnable0
+    Byte sensorEnable1
     Short levelMax0
     Short levelMax1
     Byte dLevelMaxPrecent0
@@ -47,14 +47,14 @@ class PresetParserService {
     Integer sim0MCCMNC
     Integer[] sim1MCCMNC = new Integer[70]
 
-    Short[] crc16
+    Short crc16
 
     /**
      * Load binary file
      * @param fileName
      * @return
      */
-    byte[] readFile(String fileName){
+    void readFile(String fileName) {
 
         byte[] fileData
 
@@ -62,11 +62,11 @@ class PresetParserService {
             Path path = FileSystems.getDefault().getPath(fileName)
             fileData = Files.readAllBytes(path)
 
-            if (fileData.length != FILE_LENGTH){
+            if (fileData.length != FILE_LENGTH) {
                 println("File length is incorrect must be ${FILE_LENGTH} but length is ${fileData.length}")
                 throw new Exception("File length is incorrect must be ${FILE_LENGTH} but length is ${fileData.length}")
             }
-        }catch (Exception e){
+        } catch (Exception e) {
 
             println(e.message)
             println("Create default")
@@ -74,12 +74,108 @@ class PresetParserService {
             fileData = new byte[FILE_LENGTH]
         }
 
-        return fileData
+        Arrays.fill(presetsBinary, (byte) 0);
+        presetsBinary = fileData
+    }
+
+    /**
+     * deserealize file to model
+     */
+    void toPresetModel() {
+
+        Integer j = 0
+
+        for(int i; i<sim0Apn.length; i++){
+            sim0Apn[i] = presetsBinary[j++]
+        }
+        for(int i; i<sim0ApnUser.length; i++){
+            sim0ApnUser[i] = presetsBinary[j++]
+        }
+        for(int i; i<sim0ApnPassword.length; i++){
+            sim0ApnPassword[i] = presetsBinary[j++]
+        }
+        for(int i; i<sim1Apn.length; i++){
+            sim1Apn[i] = presetsBinary[j++]
+        }
+        for(int i; i<sim1ApnUser.length; i++){
+            sim1ApnUser[i] = presetsBinary[j++]
+        }
+        for(int i; i<sim1ApnPassword.length; i++){
+            sim1ApnPassword[i] = presetsBinary[j++]
+        }
+        for(int i; i<serverURL.length; i++){
+            serverURL[i] = presetsBinary[j++]
+        }
+        for(int i; i<serverPort.length; i++){
+            serverPort[i] = presetsBinary[j++]
+        }
+        sensorEnable0 = presetsBinary[j++]
+        sensorEnable1 = presetsBinary[j++]
+
+        levelMax0 = presetsBinary[j++]>>8
+        levelMax0 |= presetsBinary[j++]
+        levelMax1 = presetsBinary[j++]>>8
+        levelMax1 |= presetsBinary[j++]
+
+        dLevelMaxPrecent0 = presetsBinary[j++]
+        dLevelMaxPrecent1 = presetsBinary[j++]
+
+        deviceID = presetsBinary[j++]>>24
+        deviceID |= presetsBinary[j++]>>16
+        deviceID |= presetsBinary[j++]>>8
+        deviceID |= presetsBinary[j++]
+
+        sim0MCCMNC = presetsBinary[j++]>>24
+        sim0MCCMNC |= presetsBinary[j++]>>16
+        sim0MCCMNC |= presetsBinary[j++]>>8
+        sim0MCCMNC |= presetsBinary[j++]
+
+        for(int i; i<sim1MCCMNC.length; i++){
+            sim1MCCMNC[i] = presetsBinary[j++]>>24
+            sim1MCCMNC[i] |= presetsBinary[j++]>>16
+            sim1MCCMNC[i] |= presetsBinary[j++]>>8
+            sim1MCCMNC[i] |= presetsBinary[j++]
+        }
+
+        crc16 = presetsBinary[j++]>>8
+        crc16 |= presetsBinary[j]
+    }
+
+    /**
+     * deserialize file to ui preset model (strings)
+     * @return
+     */
+    UIPresetsModel toUIPresetModel(){
+
+        toPresetModel()
+        UIPresetsModel uiPresetsModel = new UIPresetsModel(
+                sim0Apn: sim0Apn.toString(),
+                sim0ApnUser: sim0ApnUser.toString(),
+                sim0ApnPassword: sim0ApnPassword.toString(),
+                sim1Apn: sim0Apn.toString(),
+                sim1ApnUser: sim0ApnUser.toString(),
+                sim1ApnPassword: sim0ApnPassword.toString(),
+                serverURL: serverURL.toString(),
+                serverPort: serverPort.toString(),
+                sensorEnable0: sensorEnable0.toString(),
+                sensorEnable1: sensorEnable1.toString(),
+                levelMax0: levelMax0.toString(),
+                levelMax1: levelMax1.toString(),
+                dLevelMaxPrecent0: dLevelMaxPrecent0.toString(),
+                dLevelMaxPrecent1: dLevelMaxPrecent1.toString(),
+                deviceID: deviceID.toString(),
+                sim0MCCMNC: sim0MCCMNC.toString(),
+        )
+
+        for(int i; i<70; i++){
+            uiPresetsModel.sim1MCCMNC.add(sim1MCCMNC[i].toString())
+        }
+
+        return uiPresetsModel
     }
 
 
-
-
-
-
 }
+
+
+
